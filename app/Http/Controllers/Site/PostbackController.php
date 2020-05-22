2f568d4e6ed7566e096cfe345c6c14b305f96ee3
Guido Cerqueira <guidocerqueira@hotmail.com>
2020-05-22 15:24:39 -0300

    teste postback

--- app/Http/Controllers/Site/PostbackController.php
+++ app/Http/Controllers/Site/PostbackController.php
@@ -41,8 +41,41 @@ class PostbackController extends Controller
         if (!is_null($subscription)) {
             $subscription->status = $request->all()['subscription']['status'];
             $subscription->save();
+
+            $current_transaction = $request->all()['subscription']['current_transaction'];
+
+            $neWtransaction = Transaction::where('transaction_code', $current_transaction['id'])->first();
+
+            if (is_null($neWtransaction)) {
+                Transaction::create($this->managerTransactionData($current_transaction));
+            }
         }
 
         return;
+    }
+
+    private function managerTransactionData($transaction)
+    {
+        return [
+            'transaction_code' => $transaction['id'],
+            'status' => $transaction['status'],
+            'authorization_code' => $transaction['authorization_code'],
+            'amount' => $transaction['amount'],
+            'authorized_amount' => $transaction['authorized_amount'],
+            'paid_amount' => $transaction['paid_amount'],
+            'refunded_amount' => $transaction['refunded_amount'],
+            'installments' => $transaction['installments'],
+            'cost' => $transaction['cost'],
+            'subscription_code' => $transaction['subscription_id'],
+            'postback_url' => $transaction['postback_url'],
+            'card_holder_name' => $transaction['card_holder_name'],
+            'card_last_digits' => $transaction['card_last_digits'],
+            'card_first_digits' => $transaction['card_first_digits'],
+            'card_brand' => $transaction['card_brand'],
+            'payment_method' => $transaction['payment_method'],
+            'boleto_url' => $transaction['boleto_url'],
+            'boleto_barcode' => $transaction['boleto_barcode'],
+            'boleto_expiration_date' => date('Y-m-d H:i:s', strtotime($transaction['boleto_expiration_date']))
+        ];
     }
 }
