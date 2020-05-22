2390f52c5f27028be8576bb60c58efdc8fd1fa7f
Guido Cerqueira <guidocerqueira@hotmail.com>
2020-05-22 12:42:29 -0300

    teste postback

--- app/Http/Controllers/Site/PostbackController.php
+++ app/Http/Controllers/Site/PostbackController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 
 class PostbackController extends Controller
 {
     public function postback(Request $request)
     {
-        dd($request);
+        DB::table('postbacks')->insert([
+            'postback' => json_encode($request->all())
+        ]);
         return;
     }
 }
